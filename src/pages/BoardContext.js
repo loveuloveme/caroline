bc81b5dfc60caca8a6fb7769c21bc2a5ae8b05f8
loveuloveme <olegpodkolzin@icloud.com>
2022-10-01 04:01:49 +0300

    state selector changes

--- src/pages/BoardContext.js
+++ src/pages/BoardContext.js
@@ -36,7 +36,7 @@ export default function BoardProvider({ children }){
         setQuery(prev => {
             return {
                 ...prev,
-                state: toggleArray([...prev.state], stateId)
+                state: [stateId]
             }
         })
     };
